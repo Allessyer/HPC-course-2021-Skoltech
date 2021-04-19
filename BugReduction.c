f14153538e5c2d0fbfae73ec7d672e9df537f160
Allessyer <71093827+Allessyer@users.noreply.github.com>
2021-04-19 17:25:55 +0300

    Updated BugReduction.c

--- BugReduction.c
+++ BugReduction.c
@@ -9,38 +9,64 @@ double dotprod(double * a, double * b, size_t N)
     int i, tid;
     double sum;
 
-    tid = omp_get_thread_num();
+    //tid = omp_get_thread_num();
 
-#pragma omp parallel for reduction(+:sum)
+#pragma omp parallel for private(tid) reduction(+:sum) 
     for (i = 0; i < N; ++i)
     {
+        tid = omp_get_thread_num();
         sum += a[i] * b[i];
         printf("tid = %d i = %d\n", tid, i);
     }
-//    cout << "From dotprod function: " << sum << endl;
 
     return sum;
 }
 
 int main (int argc, char *argv[])
 {
-    const size_t N = 100;
+    const size_t N = 20;
     int i;
     double sum;
     double a[N], b[N];
-
-
+    
+    double start, end;
+    
+    cout << "N = " << N << endl;
+    
     for (i = 0; i < N; ++i)
     {
-        a[i] = b[i] = (double)i;
+        a[i] = b[i] = (double)i + 1;
     }
+    
 
     sum = 0.0;
-    	
-#pragma omp parallel shared(sum)
+    
+    cout << "Sequential part: " << endl;
+    
+    start = omp_get_wtime();
+    for (i = 0; i < N; ++i)
+    {
+    	sum += a[i] * b[i];
+    }
+    
+    cout << "Sequential SUM = " << sum << endl;
+    end = omp_get_wtime();
+    
+    cout << "Sequential Time: " << end - start << endl;
+    cout << endl;
+    
+    sum = 0.0;
+    
+    cout << "Parallel part: " << endl;
+    
+    start = omp_get_wtime();	
+    //#pragma omp parallel
     sum = dotprod(a, b, N);
-
-    printf("Sum = %f\n",sum);
+    
+    cout << "Parallel SUM = " << sum << endl;
+    end = omp_get_wtime();
+    cout << "Parallel Time: " << end - start << endl;
+    
 
     return 0;
 }
