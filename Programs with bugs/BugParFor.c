b21efa02512aad2a1e5f72c8608ee27794dbe3d0
Allessyer <71093827+Allessyer@users.noreply.github.com>
2021-04-19 19:25:53 +0300

    Updated BugParFor.c

--- Programs with bugs/BugParFor.c
+++ Programs with bugs/BugParFor.c
@@ -4,28 +4,44 @@
 
 int main (int argc, char *argv[])
 {
-    const size_t N = 100;
+    const size_t N = 5;
     const size_t chunk = 3;
 
     int i, tid;
     float a[N], b[N], c[N];
+    double start, end;
 
     for (i = 0; i < N; ++i)
     {
         a[i] = b[i] = (float)i;
     }
-
-#pragma omp parallel for shared(a,b,c,chunk), private(i,tid),schedule(static,chunk)
-    for(i = 0; i < N; i++)
+    
+    printf("Sequential part: \n\n");
+    start = omp_get_wtime();
+    for (i = 0; i < N; ++i)
     {
-        tid = omp_get_thread_num();
+        c[i] = a[i] + b[i];
+        printf("c[%d] = %f\n", i, c[i]);
+    }
+    end = omp_get_wtime();
+    printf("Sequential Time: %f\n",end - start);
+    
+    printf("\n");
+  
+    printf("Parallel part: \n");
+    printf("N = %ld, chunk = %ld\n\n",N,chunk);
+    start = omp_get_wtime();
 
-        for (i = 0; i < N; ++i)
-        {
-            c[i] = a[i] + b[i];
-            printf("tid = %d, c[%d] = %f\n", tid, i, c[i]);
-        }
-    } 
+#pragma omp parallel for default(none) shared(a,b,c,N,chunk) private(tid) schedule(static,chunk)
+for (i = 0; i < N; ++i)
+{
+    tid = omp_get_thread_num();
+    c[i] = a[i] + b[i];
+    printf("tid = %d, c[%d] = %f\n", tid, i, c[i]);
+}
+
+    end = omp_get_wtime();
+    printf("Parallel Time: %f\n",end - start);
 
     return 0;
 }
